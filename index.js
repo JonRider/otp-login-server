393fe6a18818e7a243f8473be18aaef496dc12d0
JonRider <jon@rider.net>
2022-03-27 18:34:11 -0400

    cors

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://otp-login-page.herokuapp.com/",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("A private API to request and verify One Time Passwords.");
