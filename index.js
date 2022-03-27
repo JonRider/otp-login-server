a37d512a0f97b78daa6e39ab3ed20430649c7a43
JonRider <jon@rider.net>
2022-03-27 18:20:19 -0400

    Allow requests from frontend only

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://otp-login-page.herokuapp.com/",
+  })
+);
 
 app.get("/", (req, res) => {
   res.json("A private API to request and verify One Time Passwords.");
